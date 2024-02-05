e5a4fa6b7cba67445f2ebf8bb120abdb261f6418
Ben Frame <brewerytattooer@gmail.com>
2024-02-05 11:35:37 -0500

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,29 @@
 // Test away!
+function foo () {
+    return 'the foo'
+}
+
+describe('our first test', () => {
+    test('sanity', () => {
+        expect(5).toBe(5)
+        expect(5).toBe(3 + 2)
+        expect(5).toBeDefined()
+        expect(5).toBeGreaterThan(4)
+    })
+    test('objects', () => {
+        const o = { a: 1}
+        const oo = { a: 1}
+        const ooo = oo
+        expect(o).toBe(o)
+        expect(oo).toBe(ooo)
+    })
+    test('objects again', () => {
+        expect({a:1}).toEqual({a:1})
+    })
+})
+describe('foo function', () => {
+    test('foo returns the foo', () => {
+        expect(foo()).toBe('the foo')
+        expect(foo()).toHaveLength(7)
+    })
+})
